1347dde7e6b1916cf99be3a80f66d9db611a7acc
sterenz <87770613+sterenz@users.noreply.github.com>
2023-11-06 18:44:26 +0100

    Created clean D3 json and updated `Numbers.tsx` component

--- empower-italy/README.md
+++ empower-italy/README.md
@@ -2,21 +2,19 @@ This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next
 
 ## Getting Started
 
-First, run the development server:
+First, install the needed modules:
 
 ```bash
-npm run dev
-# or
+yarn install
+```
+
+Then, run the development server:
+
+```bash
 yarn dev
-# or
-pnpm dev
-# or
-bun dev
 ```
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
 
 This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
 
--- empower-italy/components/Numbers.tsx
+++ empower-italy/components/Numbers.tsx
@@ -11,57 +11,110 @@ interface ImmigrationData {
   Quantity: number;
 }
 
+interface PopulationEntry {
+  TIME_PERIOD: string;
+  ObsValue: string;
+}
+
 export default function Numbers() {
 
     // Funtion to calculate total foreing residents                                
     const [totalImmigrants, setTotalImmigrants] = useState<string | null>(null);
+    const [totalItalianPopulation, setTotalItalianPopulation] = useState<number | null>(null);
+    const [percentage, setPercentage] = useState<string | null>(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
 
     useEffect(() => {
-        fetch("https://raw.githubusercontent.com/openaccesstoimmigrants/openaccesstoimmigrants/main/_datasets/Clean/D1(a)/immigrants_demographic.json")
-        .then((response) => response.json())
-        .then((jsonData: ImmigrationData[]) => {
-            let maleSum = 0;
-            let femaleSum = 0;
-
-            jsonData.forEach((entry) => {
-            if (entry.Year === 2022 && entry.Territory === "IT" && entry.Age === "TOTAL") {
-                if (entry.Sex === "Male") {
-                maleSum += entry.Quantity;
-                } else if (entry.Sex === "Female") {
-                femaleSum += entry.Quantity;
+        const fetchData = async () => {
+            try {
+                // Fetch total immigrants data
+                const immigrantsResponse = await fetch("https://raw.githubusercontent.com/openaccesstoimmigrants/openaccesstoimmigrants/main/_datasets/Clean/D1(a)/immigrants_demographic.json");
+                if (!immigrantsResponse.ok) {
+                    throw new Error("Network response was not ok.");
                 }
-            }
-            });
 
-            const total = maleSum + femaleSum;
-            const totalWithCommas = total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-            setTotalImmigrants(totalWithCommas);
-        });
+                const immigrantsData: ImmigrationData[] = await immigrantsResponse.json();
+
+                // Fetch total Italian population for 2022
+                const populationResponse = await fetch("https://raw.githubusercontent.com/openaccesstoimmigrants/openaccesstoimmigrants/main/_datasets/Clean/D3/resident_italian_population.json");
+                if (!populationResponse.ok) {
+                    throw new Error("Network response was not ok.");
+                }
+                const populationData = await populationResponse.json();
+
+                // Calculate total immigrants for 2022
+                let maleSum = 0;
+                let femaleSum = 0;
+
+                immigrantsData.forEach((entry) => {
+                    if (entry.Year === 2022 && entry.Territory === "IT" && entry.Age === "TOTAL") {
+                        if (entry.Sex === "Male") {
+                            maleSum += entry.Quantity;
+                        } else if (entry.Sex === "Female") {
+                            femaleSum += entry.Quantity;
+                        }
+                    }
+                });
+
+                const totalImmigrants = maleSum + femaleSum;
+
+                // Calculate total Italian population for 2022
+                const italianPopulation2022 = populationData.IT.find(
+                    (entry: PopulationEntry) => entry.TIME_PERIOD === "2022"
+                );
+
+                const totalPopulation2022 = italianPopulation2022
+                    ? parseInt(italianPopulation2022.ObsValue, 10)
+                : 0;
+
+                // Calculate the percentage
+                const percentage = ((totalImmigrants / totalPopulation2022) * 100).toFixed(1);
+
+                setTotalItalianPopulation(totalPopulation2022);
+                setPercentage(percentage);
+                setLoading(false);
+            } catch (error) {
+                setError("An error occurred while fetching data.");
+                setLoading(false);
+            }
+        };
+
+        fetchData();
     }, []);
 
 
-  return (
-      <section id="numbers-demographic" className="pb-6">
-        <article className="max-w-7xl m-auto">
-            <div className="grid grid-cols-12 gap-6 h-full">
-                <div className="bg-indigo-100 rounded-2xl p-12 col-span-7 grid">
-                    <h1 className="font-bold text-4xl text-indigo-900 uppercase pb-8">TOTAL RESIDENT FOREIGNERS</h1>
-                    <p className="font-bold text-7xl text-indigo-600">{totalImmigrants}</p> 
-                    <p className="font-bold text-4xl text-indigo-600">in Italy in 2022</p>
-                    <Link href="/" className="place-self-end font-bold text-lg text-indigo-600 uppercase pt-6">
-                        Source
-                    </Link>
+    return (
+        <section id="numbers-demographic" className="pb-6">
+            <article className="max-w-7xl m-auto">
+                <div className="grid grid-cols-12 gap-6 h-full">
+                    <div className="bg-indigo-100 rounded-2xl p-12 col-span-7 grid">
+                        <h1 className="font-bold text-4xl text-indigo-900 uppercase pb-8">TOTAL RESIDENT FOREIGNERS</h1>
+                        {/* conditional rendering */}
+                        {loading ? (
+                            <p>Loading...</p>
+                        ) : error ? (
+                            <p>{error}</p>        
+                        ) : (
+                            <>
+                                <p className="font-bold text-7xl text-indigo-600">{totalImmigrants}</p> 
+                            </>
+                        )}
+                        <p className="font-bold text-4xl text-indigo-600">in Italy in 2022</p>
+                        <Link href="/" className="place-self-end font-bold text-lg text-indigo-600 uppercase pt-6">
+                            Source
+                        </Link>
+                    </div>
+                    <div className="bg-indigo-100 rounded-2xl p-12 col-span-5 grid">
+                        <h1 className="font-bold text-4xl text-indigo-900 uppercase pb-8">THEY REPRESENT THE</h1>
+                        <p className="font-bold text-7xl text-indigo-600">{percentage}</p> {/* data will be loaded here */}
+                        <p className="font-bold text-4xl text-indigo-600">of the Italian population</p>
+                        <Link href="/" className="place-self-end font-bold text-lg text-indigo-600 uppercase pt-6">
+                            Source
+                        </Link>
+                    </div>
                 </div>
-                <div className="bg-indigo-100 rounded-2xl p-12 col-span-5 grid">
-                    <h1 className="font-bold text-4xl text-indigo-900 uppercase pb-8">THEY REPRESENT</h1>
-                    <p className="font-bold text-7xl text-indigo-600">6,2%</p> {/* data will be loaded here */}
-                    <p className="font-bold text-4xl text-indigo-600">of the Italian population</p>
-                    <Link href="/" className="place-self-end font-bold text-lg text-indigo-600 uppercase pt-6">
-                        Source
-                    </Link>
-                </div>
-            </div>
-        </article>
-      </section>
-  )
+            </article>
+        </section>
+    )
 }
