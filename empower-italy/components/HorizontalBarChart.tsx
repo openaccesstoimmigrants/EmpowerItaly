41991749da368901f3201ad46ee3cb12c37ff16a
sterenz <87770613+sterenz@users.noreply.github.com>
2023-12-07 00:50:23 +0100

    Updated `HorizontalBarChart.tsx`

--- empower-italy/components/HorizontalBarChart.tsx
+++ empower-italy/components/HorizontalBarChart.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Link from "next/link";
 import {
     Chart as ChartJS,
@@ -15,188 +15,204 @@ import { Bar } from 'react-chartjs-2';
 
 ChartJS.register(BarElement, CategoryScale, LinearScale, Title, Tooltip, Legend);
 
+interface OccupationDataAll {
+  foreign: OccupationData[];
+  italian: OccupationData[];
+}
+
 interface OccupationData {
-    Occupation: string;
-    Year: string;
-    Value: number;
+  OCCUPATION: string;
+  YEAR: string;
+  VALUE: number;
 }
 
 export default function HorizontalBarChart() {
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+    const [jsonData, setJsonData] = useState<OccupationDataAll | null>(null);
+    const [occupation, setOccupation] = useState<string>('industry');
+    const [chartData, setChartData] = useState<{ labels: string[]; datasets: any[] }>({
+        labels: [],
+        datasets: [],
+    });
 
-    const [jsonData, setJsonData] = useState<OccupationData[]>([]);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const response = await fetch('https://raw.githubusercontent.com/openaccesstoimmigrants/openaccesstoimmigrants/main/_datasets/Clean/D6/occupations_rate.json');
                 if (!response.ok) {
-                    throw new Error("Network response was not ok.")
+                throw new Error("Network response was not ok.")
                 }
 
-                const data = await response.json() as OccupationData[];
-                setJsonData(data);
+                const data = await response.json();
+                setJsonData(data as OccupationDataAll);
+                setLoading(false);
 
             } catch (error) {
                 console.error("Error fetching data:", error);
+                setError("An error occurred while fetching data.");
+                setLoading(false);
             }
         };
 
         fetchData();
     }, []);
 
-
-    const years = Array.from(new Set(jsonData.map(item => item.Year.toString())));
-    const occupations = Array.from(new Set(jsonData.map(item => item.Occupation)));
-
-    const groupedData: Record<string, Record<string, number[]>> = {};
-    jsonData.forEach(item => {
-        if (!groupedData[item.Occupation]) {
-            groupedData[item.Occupation] = {};
+    const getChartData = useCallback(() => {
+        if (!jsonData) {
+            console.log('Data not available.');
+            return { labels: [], datasets: [] };
         }
-        if (!groupedData[item.Occupation][item.Year]) {
-            groupedData[item.Occupation][item.Year] = [];
-        }
-        groupedData[item.Occupation][item.Year].push(item.Value);
-    });
 
+        const italianOccupationData = jsonData.italian?.filter(data => data.OCCUPATION === occupation) || [];
+        const foreignOccupationData = jsonData.foreign?.filter(data => data.OCCUPATION === occupation) || [];
 
-    // Define custom colors for bars
-    const customColors = [
-        'rgba(244, 63, 94, 0.6)', // rose
-        'rgba(217, 70, 239, 0.6)', // fucsia
-        'rgba(139, 92, 246, 0.6)', // violet
-        'rgba(59, 130, 246, 0.6)', // blue
-        'rgba(6, 182, 212, 0.6)', // cyan
-        'rgba(16, 185, 129, 0.6)', // emerald
-    ];
+        const years = italianOccupationData.map(data => data.YEAR);
+        const italianValues = italianOccupationData.map(data => data.VALUE);
+        const foreignValues = foreignOccupationData.map(data => data.VALUE);
 
-    const customBorderColors = [
-        'rgba(244, 63, 94, 0.6)', // rose
-        'rgba(217, 70, 239, 0.6)', // fucsia
-        'rgba(139, 92, 246, 0.6)', // violet
-        'rgba(59, 130, 246, 0.6)', // blue
-        'rgba(6, 182, 212, 0.6)', // cyan
-        'rgba(16, 185, 129, 0.6)', // emerald
-    ];
-
-
-    const datasets = occupations.map((occupation, index) => {
-        const data = years.map(year => {
-            const total = groupedData[occupation][parseInt(year)]?.reduce((acc, val) => acc + val, 0) || 0;
-            return total;
-        });
-        return {
-            label: occupation,
-            data,
-            backgroundColor: customColors[index % customColors.length], // Assign custom colors
-            borderColor: customBorderColors[index % customBorderColors.length], // Assign custom colors
-            borderWidth: 1,
-        };
-    });
-
-    const chartData = {
+        setChartData((prevState: any) => ({
+        ...prevState,
         labels: years,
-        datasets,
-    };
+        datasets: [
+            {
+            label: 'Italian',
+            data: italianValues,
+            backgroundColor: 'rgba(79, 70, 229, 0.6)',
+            borderColor: 'rgba(79, 70, 229, 1)',
+            borderWidth: 1,
+            },
+            {
+            label: 'Foreign',
+            data: foreignValues,
+            backgroundColor: 'rgba(234, 88, 12, 0.6)',
+            borderColor: 'rgba(234, 88, 12, 1)',
+            borderWidth: 1,
+            },
+        ],
+        }));
+    }, [occupation, jsonData]);
+
+    // Update chart data when occupation or jsonData changes
+    useEffect(() => {
+        getChartData();
+    }, [getChartData]);
 
     const options = {
         maintainAspectRatio: false,
         responsive: true,
-        indexAxis: 'y' as const,
-        // plugins: {
-        //     legend: {
-        //         position: 'right' as const,
-        //         align: 'start' as const,
-        //     }
-        // },
+        //indexAxis: 'y' as const,
         scales: {
-            x: {
-                title: {
-                    display: true,
-                    text: 'Number of workers',
-                },
+        x: {
+            title: {
+            display: true,
+            text: 'Number of workers (thousand)',
             },
-            y: {
-                title: {
-                    display: false,
-                },
-                suggestedMin: 0,
+        },
+        y: {
+            title: {
+            display: false,
             },
+            suggestedMin: 11000,
+            suggestedMax: 11000, // Set a fixed maximum value for the y-axis
+        },
         },
     };
 
-    // console.log(jsonData); // Check data in the console to ensure it's fetched
+    const handleOccupationChange = (selectedOccupation: string) => {
+        setOccupation(selectedOccupation);
+    };
 
     return (
-        <section id="barchart-education" className="pb-6">
-            <article className="
-                            max-w-7xl
-                            m-auto
-                            animate-fade-in-down
-            ">
-                <div className="
-                            m-full
-                            gap-6
-                            h-full
-                            animate-fade-in-down
-                ">
-
-                        <div className="
-                                    bg-indigo-100
-                                    rounded-2xl
-                                    p-8
-                                    md:p-12
-                                    row-span-4
-                                    grid
-                                    hover:drop-shadow-md
-                                    transition-all
-                        ">
-                            <div className="
-                                        grid
-                                        grid-cols-1
-                                        lg:grid-cols-2
-                                        gap-6
-                                        pb-6
-                            ">
-                                <h1 className="
-                                            font-bold
-                                            text-4xl
-                                            lg:text-6xl
-                                            text-indigo-900
-                                            pb-8
-                                ">
-                                    Occupation distribution
-                                </h1>
-                                <p className="
-                                            text-lg
-                                ">
-                                    Lorem lorem
-                                </p>
-
-                            </div>
-
-                            <div id="chart-container" className="
-                                                            relative
-                                                            m-auto
-                                                            w-full
-                                                            h-[42rem]
-                                                            bg-gray-50
-                                                            rounded-xl
-                                                            px-2 py-6
-                                                            
-                            ">
-                                {jsonData.length > 0 ? (
-                                    <Bar data={chartData} options={options} />
-                                ) : (
-                                    <p>Loading...</p>
-                                )}
-                            </div>
-                        <Link href="/" className="place-self-end type-button">
-                            Source DX
-                        </Link>
-                        </div>
-                    </div>
-            </article>
-        </section>
-    )
+        <div>
+        <select onChange={(e) => handleOccupationChange(e.target.value)}>
+            <option value="industry">Industry</option>
+            <option value="other services activity">Other Services Activity</option>
+            <option value="trade, hotels and restaurants">Trade, Hotels and Restaurants</option>
+            <option value="agriculture, forestry and fishing">Agriculture, Forestry and Fishing</option>
+            <option value="construction">Construction</option>
+        </select>
+        <div>
+            <h2>Occupation: {occupation}</h2>
+                                <div className="h-[32rem] m-2">
+            <Bar data={chartData} options={options} />
+</div>
+        </div>
+        </div>
+    );
 }
+
+
+    // return (
+    //     <section id="barchart-education" className="pb-6">
+    //         <article className="
+    //                         max-w-7xl
+    //                         m-auto
+    //                         animate-fade-in-down
+    //         ">
+    //             <div className="
+    //                         m-full
+    //                         gap-6
+    //                         h-full
+    //                         animate-fade-in-down
+    //             ">
+
+    //                     <div className="
+    //                                 bg-indigo-100
+    //                                 rounded-2xl
+    //                                 p-8
+    //                                 md:p-12
+    //                                 row-span-4
+    //                                 grid
+    //                                 hover:drop-shadow-md
+    //                                 transition-all
+    //                     ">
+    //                         <div className="
+    //                                     grid
+    //                                     grid-cols-1
+    //                                     lg:grid-cols-2
+    //                                     gap-6
+    //                                     pb-6
+    //                         ">
+    //                             <h1 className="
+    //                                         font-bold
+    //                                         text-4xl
+    //                                         lg:text-6xl
+    //                                         text-indigo-900
+    //                                         pb-8
+    //                             ">
+    //                                 Occupation distribution
+    //                             </h1>
+    //                             <p className="
+    //                                         text-lg
+    //                             ">
+    //                                 Lorem lorem
+    //                             </p>
+
+    //                         </div>
+
+    //                         <div id="chart-container" className="
+    //                                                         relative
+    //                                                         m-auto
+    //                                                         w-full
+    //                                                         h-[42rem]
+    //                                                         bg-gray-50
+    //                                                         rounded-xl
+    //                                                         px-2 py-6
+                                                            
+    //                         ">
+    //                             {jsonData.foreign.length > 0 || jsonData.italian.length > 0 ? (
+    //                                 <Bar data={chartData} options={options} />
+    //                             ) : (
+    //                                 <p>Loading...</p>
+    //                             )}
+    //                         </div>
+    //                     <Link href="/" className="place-self-end type-button">
+    //                         Source DX
+    //                     </Link>
+    //                     </div>
+    //                 </div>
+    //         </article>
+    //     </section>
+    // )
--- empower-italy/components/MashUp.tsx
+++ empower-italy/components/MashUp.tsx
@@ -62,13 +62,13 @@ export default function MashUp() {
                 }
 
                 const data: MashupData = await response.json();
-                console.log('Fetched data:', data); // Log fetched data
+                // console.log('Fetched data:', data); // Log fetched data
 
                 setJsonData(data);
                 setLoading(false); // Update isLoading once data is fetched
 
             } catch (error) {
-                console.error("Error fetching data:", error);
+                // console.error("Error fetching data:", error);
                 setError("An error occurred while fetching data.");
                 setLoading(false);
             }
@@ -232,7 +232,7 @@ export default function MashUp() {
                                     <div className="p-4 font-bold text-indigo-600">
                                         <label htmlFor="territorySelect" className="text-indigo-900">Select Territory:</label>
                                         <div className="relative inline-flex">
-                                            <svg className="w-3 h-3 absolute top-[40%] right-4 pointer-events-none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 412 232"><path d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z" fill="rgb(249 250 251)" fill-rule="nonzero"/></svg>
+                                            <svg className="w-3 h-3 absolute top-[40%] right-4 pointer-events-none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 412 232"><path d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z" fill="rgb(249 250 251)"/></svg>
                                             <select id="territorySelect" className="text-gray-50 bg-indigo-300 hover:bg-indigo-500 focus:ring-2 focus:outline-none focus:ring-indigo-200 font-medium rounded-lg pl-4 pr-7 py-2 m-2 text-center inline-flex items-center appearance-none" onChange={handleTerritoryChange} value={selectedTerritory}>
                                                 <option value="center">Center</option>
                                                 <option value="south and islands">South and Islands</option>
