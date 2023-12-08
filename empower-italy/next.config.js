bddd8c8334ac8350499772a6b4c41e4239d50a11
sterenz <87770613+sterenz@users.noreply.github.com>
2023-12-08 11:35:08 +0100

    Deployment file

--- empower-italy/next.config.js
+++ empower-italy/next.config.js
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+    distDir: 'dist',
+    images: {
+        unoptimized: true
+    },
+    // experimental: {
+    //     appDir: true
+    // },
+}
+ 
 module.exports = nextConfig
