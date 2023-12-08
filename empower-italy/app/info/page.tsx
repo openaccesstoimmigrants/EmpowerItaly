214a9658a7930395714c812b99cf1750410d2376
sterenz <87770613+sterenz@users.noreply.github.com>
2023-12-08 09:12:53 +0100

    Updated `Info`

--- empower-italy/app/info/page.tsx
+++ empower-italy/app/info/page.tsx
@@ -32,9 +32,21 @@ export default function page() {
                         ">
                             Contact
                         </h1>
-                        <p>
-                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam distinctio quis ullam optio dignissimos? Facere ullam ea neque. Magni ab hic ipsa? Nam deleniti illo sit molestias deserunt sed pariatur!
-                        </p>
+
+                            <p>
+                                The EmpowerItaly datasets comprise information gathered from catalogs pertaining to various aspects of the Italian labor market. Initially created as part of a University of Bologna course, the catalog is presently not actively maintained. Nevertheless, the datasets within this catalog are managed by their respective institutions. Despite the catalog's lack of ongoing updates, our scripts remain accessible and can be executed on new files whenever needed.
+                            </p>
+                            <p>
+                                If you discover a newer version of any of our input files, please notify us to assist in updating our files through our automated script. It's essential to note that our scripts are licensed under CC 4.0. We invite the community to contribute to the enhancement of our files by submitting updated versions to our <Link className="font-bold underline" href="https://github.com/openaccesstoimmigrants/openaccesstoimmigrants">GitHub</Link> project. Submitted files will undergo review, and if deemed accurate, they will be added to the project.
+                            </p>
+                            <p>
+                                For further communication, you can reach us via our university emails:
+                            </p>
+                            <ul>
+                                <li>Stefano Renzetti: <Link className="underline" href="mailto:stefano.renzetti3@studio.unibo.it">stefano.renzetti3@studio.unibo.it</Link></li>
+                                <li>Camila Oliveira: <Link className="underline" href="mailto:camila.oliveira@studio.unibo.it">camila.oliveira@studio.unibo.it</Link></li>
+                                <li>Giusy Grieco: <Link className="underline" href="mailto:giusy.grieco@studio.unibo.it">giusy.grieco@studio.unibo.it</Link></li>
+                            </ul>
                     </div>
                 </div>
             </article>
